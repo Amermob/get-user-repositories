0278916cf43777ac191ed8fbfccad97571399d15
Amer Mohamed <96571594+Amermob@users.noreply.github.com>
2024-07-07 00:13:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+//select the main variables
+
+const theInput = document.querySelector(".get-repos input");
+const getRepo = document.querySelector(".get-button");
+const reposData = document.querySelector(".show-repo");
+
+//  by pressing on the getrepo button the function
+// will start another function which is the main function
+getRepo.onclick = function () {
+  // main function
+  getRepos();
+};
+
+function getRepos() {
+  //if the
+  if (theInput.value == "") {
+    //if input value is empty then put a text to tell the user
+    reposData.innerHTML = "<span>Please Enter Github Username</span>";
+  } else {
+    // fetch using an api
+    fetch(`https://api.github.com/users/${theInput.value}/repos`)
+      .then((repo) => {
+        // turn to json
+        return repo.json();
+      })
+      .then((repos) => {
+        // once you recive a response then empty the "reposData"
+        reposData.innerHTML = "";
+        // theInput.value = "";
+        // loop to add all the repos into the page
+        repos.forEach((repo) => {
+          // repos Name:-
+          // creat an element
+          let mainDiv = document.createElement("div");
+          // put the text inside the element
+          mainDiv.appendChild(document.createTextNode(repo.name));
+          // append the element into the reposData
+          reposData.appendChild(mainDiv);
+          // ----------------------------------------
+          // repos Url:-
+          let repoUrl = document.createElement("a");
+          // creat repo url
+          repoUrl.appendChild(document.createTextNode("Visit"));
+          // append the url text to anchor tag
+          repoUrl.href = `https://github.com/${theInput.value}/${repo.name}`;
+          // add the hypertext refrence "href"
+          repoUrl.setAttribute("target", "_blank");
+          // set attribute so the link open in a new tap
+          mainDiv.appendChild(repoUrl);
+          // append url anchor to main Div
+          // ----------------------------------------
+          // repo star:-
+          // creat stars count span
+          let repoStar = document.createElement("span");
+          // append star count text to stars span
+          repoStar.appendChild(
+            document.createTextNode(` stars: ${repo.stargazers_count}`)
+          );
+          // append stars count span to main Div
+          mainDiv.appendChild(repoStar);
+          // ----------------------------------------
+          // add class for the main div to style it better
+          mainDiv.className = "repo-box";
+          // ----------------------------------------
+          // make the info dynamic which means any github user can it
+          console.log(repo.name);
+        });
+        theInput.value = "";
+      });
+  }
+}
